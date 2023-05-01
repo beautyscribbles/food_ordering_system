925bc605bcef9c862d30d17d132a1e410b8d9570
flh6 <talk2florencehanson@gmail.com>
2023-05-01 07:57:23 -0700

    boilgerplate.c

--- main/3-boilerplate.c
+++ main/3-boilerplate.c
@@ -1,28 +1,27 @@
 // C program to implement Boilerplate
 // // Code for the Food Ordering System
-// int main()
-// {
-//     while (1) {
-//             printf("\n\nWelcome to Food ");
-//              
-//                      printf("Ordering System"
-//                                     "\n\n1)SIGNUP\n");
-//                                             printf("2)LOGIN\n3)EXIT\n\n");
-//                                                     printf("Enter your choice\t");
-//                                                             scanf("%d", &choice);
-//                                                              
-//                                                                      // Switch Cases
-//                                                                              switch (choice) {
-//                                                                               
-//                                                                                       // For Signup Function
-//                                                                                               case 1: {
-//                                                                                                           signup();
-//                                                                                                                       break;
-//                                                                                                                               }
-//                                                                                                                                
-//                                                                                                                                        // For Login Function
-//                                                                                                                                                case 2: {
-//                                                                                                                                                            login();
+int main()
+{
+while (1) {
+printf("\n\nWelcome to Food ");
+           
+printf("Ordering System"
+"\n\n1)SIGNUP\n");
+printf("2)LOGIN\n3)EXIT\n\n");
+printf("Enter your choice\t");
+scanf("%d", &choice);
+                                                              
+                                                                      // Switch Cases
+switch (choice) {
+                                                                               
+// For Signup Function
+case 1: {
+signup();
+break;
+	}                                                                                                                           
+                                                                     // For Login Function
+case 2: {
+                                                                                                                                               login();
 //                                                                                                                                                                        break;
 //                                                                                                                                                                                }
 //                                                                                                                                                                                        case 3: {
@@ -32,7 +31,7 @@
 //                                                                                                                                                                                                                                    printf("\nPlease Enter the "
 //                                                                                                                                                                                                                                                    printf("valid choice\n");
 //                                                                                                                                                                                                                                                                    break;
-//                                                                                                                                                                                                                                                                            }
+//                                                                                                                                                                                                                                                           }
 //                                                                                                                                                                                                                                                                                    }
 //                                                                                                                                                                                                                                                                                        }
 //                                                                                                                                                                                                                                                                                        }
