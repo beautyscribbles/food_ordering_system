0bd346a9c734f2c9a7183682e405321c4f5695be
flh6 <talk2florencehanson@gmail.com>
2023-05-01 11:12:08 -0700

    updated boilerplate.c

--- main/3-boilerplate.c
+++ main/3-boilerplate.c
@@ -3,13 +3,13 @@
 int main()
 {
 while (1) {
-printf("\n\nWelcome to Food ");
+ printf("\n\nWelcome to Food ");
            
-printf("Ordering System"
-"\n\n1)SIGNUP\n");
-printf("2)LOGIN\n3)EXIT\n\n");
-printf("Enter your choice\t");
-scanf("%d", &choice);
+ printf("Ordering System"
+ "\n\n1)SIGNUP\n");
+ printf("2)LOGIN\n3)EXIT\n\n");
+ printf("Enter your choice\t");
+ scanf("%d", &choice);
                                                               
                                                                       // Switch Cases
 switch (choice) {
@@ -26,11 +26,11 @@ break;
 }
                                                                      case 3: {
 exit(1);
-}                                                                                                                                                                                  default: {
-//                                                                                                                                                                                                                                    printf("\nPlease Enter the "
-//                                                                                                                                                                                                                                                    printf("valid choice\n");
-                                                                                                                                                                                                                                                             break;
-}
-}
-}
+}                                                                    default: {
+   printf("\nPlease Enter the ")
+   printf("valid choice\n");
+break;
+    }
+  }
+ }
 }
