33d02747c190112a36f3c132fdaeebf8343f6b2e
flh6 <talk2florencehanson@gmail.com>
2023-05-01 11:15:26 -0700

    added commits to boilerplate.cd

--- main/3-boilerplate.c
+++ main/3-boilerplate.c
@@ -1,5 +1,5 @@
 // C program to implement Boilerplate
-// // Code for the Food Ordering System
+// Code for the Food Ordering System
 int main()
 {
 while (1) {
@@ -15,17 +15,16 @@ while (1) {
 switch (choice) {
                                                                                
 // For Signup Function
-case 1: {
-signup();
-break;
+  case 1: {
+  signup();
+  break;
 	}                                                                                                                           
                                                                      // For Login Function
-case 2: {
-login();
-break;
-}
-                                                                     case 3: {
-exit(1);
+  case 2: {
+  login();
+  break;
+}                                                                     case 3: {
+  exit(1);
 }                                                                    default: {
    printf("\nPlease Enter the ")
    printf("valid choice\n");
